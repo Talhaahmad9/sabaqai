56816aa7d30afca998e529ce7a85dc0ebe775334
Talhaahmad9 <talhaahmaed999@gmail.com>
2026-03-13 17:36:04 +0500

    fix middleware

--- middleware.js
+++ middleware.js
@@ -7,5 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/chat/:path*", "/onboarding/:path*"],
+  matcher: ["/dashboard/:path*", "/chat/:path*"],
 };
